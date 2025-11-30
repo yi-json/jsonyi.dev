d8c413af00aa5df231ec8a56337dbf76a71f92b7
Jason Yi <jasonyi@unc.edu>
2025-11-30 15:17:50 -0500

    fix light/dark mode toggle

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,12 +3,12 @@
 // Toggle through light, dark, and system theme settings.
 let toggleThemeSetting = () => {
   let themeSetting = determineThemeSetting();
-  if (themeSetting == "system") {
+  let computedTheme = determineComputedTheme();
+
+  if (computedTheme == "dark") {
     setThemeSetting("light");
-  } else if (themeSetting == "light") {
-    setThemeSetting("dark");
   } else {
-    setThemeSetting("system");
+    setThemeSetting("dark");
   }
 };
 
